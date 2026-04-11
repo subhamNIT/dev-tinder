4f7708484a3c81ea25003ecdc35052f1b595c8d9
Subham Gupta <subham.guptasg05@gmail.com>
2026-04-11 16:41:57 +0530

    Enable cookue handling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const authRouter = require('./routes/auth')
 const profileRouter = require('./routes/profile')
 const requestsRouter = require('./routes/requests');
@@ -8,6 +9,10 @@ const userRouter = require('./routes/user');
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true //require to save the token in the cookies
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
